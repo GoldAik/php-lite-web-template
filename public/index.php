2fa56727513660cd9cd8b6ebe7855a5b52c6b42d
GoldAik <aik0124k@gmail.com>
2025-07-04 17:11:55 +0200

    add middleware

--- public/index.php
+++ public/index.php
@@ -7,10 +7,12 @@ define('DEBUG_MODE', true);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->addRoutingMiddleware();
 
 $app->addErrorMiddleware(
     displayErrorDetails: DEBUG_MODE,
@@ -18,9 +20,16 @@ $app->addErrorMiddleware(
     logErrorDetails: true,
 );
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$middleware = function (Request $req, RequestHandler $reqHandler) use ($app) {
+    $res = $reqHandler->handle($req);
+    $res->getBody()->write('after');
+    return $res;
+};
+
+$app->get('/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello $name");
     return $response;
-});
+})->add($middleware);
 
 $app->run();
