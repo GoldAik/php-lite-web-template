09885dfb97850317bf56acf8f723a47d0c014757
GoldAik <aik0124k@gmail.com>
2025-07-04 17:46:38 +0200

    add php-di

--- public/index.php
+++ public/index.php
@@ -6,12 +6,12 @@ define('DEBUG_MODE', true);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+
+$app = \DI\Bridge\Slim\Bridge::create();
 $app->addRoutingMiddleware();
 
 $app->addErrorMiddleware(
@@ -20,14 +20,13 @@ $app->addErrorMiddleware(
     logErrorDetails: true,
 );
 
-$middleware = function (Request $req, RequestHandler $reqHandler) use ($app) {
+$middleware = function (Request $req, RequestHandler $reqHandler) {
     $res = $reqHandler->handle($req);
-    $res->getBody()->write('after');
+    $res->getBody()->write(' - after middleware');
     return $res;
 };
 
-$app->get('/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
+$app->get('/{name}', function (Response $response, $name) {
     $response->getBody()->write("Hello $name");
     return $response;
 })->add($middleware);
