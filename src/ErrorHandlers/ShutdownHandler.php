1e14288875f4078be623202eff793a6c08cb3b3f
GoldAik <aik0124k@gmail.com>
2025-07-21 14:25:35 +0200

    configure error displaying for production

--- src/ErrorHandlers/ShutdownHandler.php
+++ src/ErrorHandlers/ShutdownHandler.php
@@ -10,61 +10,65 @@ use Slim\ResponseEmitter;
 
 class ShutdownHandler
 {
-
-    private Request $request;
-    private HttpErrorHandler $errorHandler;
-    private bool $displayErrorDetails;
-
     /**
      * ShutdownHandler constructor.
      */
-    public function __construct(Request $request, HttpErrorHandler $errorHandler, bool $displayErrorDetails) {
-        $this->request = $request;
-        $this->errorHandler = $errorHandler;
-        $this->displayErrorDetails = $displayErrorDetails;
-    }
+    public function __construct(
+        private Request $request,
+        private HttpErrorHandler $errorHandler,
+        private bool $displayErrorDetails,
+        private int $ignoreErrors = E_NOTICE | E_WARNING,
+        private bool $ignoreErrorsOnDisplayDetails = false,
+    ) { }
 
     public function __invoke()
-    {
+    {        
         $error = error_get_last();
-        if ($error) {
-            $errorFile = $error['file'];
-            $errorLine = $error['line'];
-            $errorMessage = $error['message'];
-            $errorType = $error['type'];
-            $message = 'An error while processing your request. Please try again later.';
-
-            if ($this->displayErrorDetails) {
-                switch ($errorType) {
-                    case E_USER_ERROR:
-                        $message = "FATAL ERROR: {$errorMessage}. ";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
-
-                    case E_USER_WARNING:
-                        $message = "WARNING: {$errorMessage}";
-                        break;
-
-                    case E_USER_NOTICE:
-                        $message = "NOTICE: {$errorMessage}";
-                        break;
-
-                    default:
-                        $message = "ERROR: {$errorMessage}";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
-                }
-            }
-
-            $exception = new HttpInternalServerErrorException($this->request, $message);
-            $response = $this->errorHandler->__invoke($this->request, $exception, $this->displayErrorDetails, false, false);
-            
-            if (ob_get_length()) {
-              ob_clean();
-            }
-
-            $responseEmitter = new ResponseEmitter();
-            $responseEmitter->emit($response);
+        if (! $error) {
+            return;
         }
+        
+        $errorFile = $error['file'];
+        $errorLine = $error['line'];
+        $errorMessage = $error['message'];
+        $errorType = $error['type'];
+        $message = 'An error while processing your request. Please try again later.';
+        
+        if ((! $this->displayErrorDetails || $this->ignoreErrorsOnDisplayDetails)
+                && ($errorType & $this->ignoreErrors) === $errorType) {
+            return;
+        }
+
+        if ($this->displayErrorDetails) {
+            switch ($errorType) {
+                case E_USER_ERROR:
+                    $message = "FATAL ERROR: {$errorMessage}. ";
+                    $message .= " on line {$errorLine} in file {$errorFile}.";
+                    break;
+
+                case E_USER_WARNING:
+                    $message = "WARNING: {$errorMessage}";
+                    break;
+
+                case E_USER_NOTICE:
+                    $message = "NOTICE: {$errorMessage}";
+                    break;
+
+                default:
+                    $message = "ERROR: {$errorMessage}";
+                    $message .= " on line {$errorLine} in file {$errorFile}.";
+                    break;
+            }
+        }
+
+        $exception = new HttpInternalServerErrorException($this->request, $message);
+        $response = $this->errorHandler->__invoke($this->request, $exception, $this->displayErrorDetails, false, false);
+        
+        if (ob_get_length()) {
+            ob_clean();
+        }
+
+        $responseEmitter = new ResponseEmitter();
+        $responseEmitter->emit($response);
     }
 }
