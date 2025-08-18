56285d60dc1a36e72e8d5a3bb8998126de3edca2
GoldAik <aik0124k@gmail.com>
2025-08-18 09:56:18 +0200

    turn off route cache on debug mode

--- src/Routes/main.php
+++ src/Routes/main.php
@@ -4,16 +4,29 @@ declare(strict_types = 1);
 
 namespace App\Routes;
 
+use App\Config;
+use Psr\Http\Message\ResponseInterface as Response;
+
 return function (\Slim\App $app, $args = []) {    
     /**
      * Declaring routes
      */
+    $app->get('/', function (Response $response) {
+        $response->getBody()->write("Hello World");
+        return $response;
+    });
+
     (require_once 'user.php')($app, $args);
 
 
     /**
      * Caching routes
      */
-    $routeCollector = $app->getRouteCollector();
-    $routeCollector->setCacheFile(CACHE_PATH . '/routes.file');
+    $container = $app->getContainer();
+    $debugMode = $container->get(Config::class)['debug_mode'];
+
+    if (! $debugMode) {
+        $routeCollector = $app->getRouteCollector();
+        $routeCollector->setCacheFile(CACHE_PATH . '/routes.file');
+    }
 };
