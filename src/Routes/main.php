79cab9b5d0821cbeb945cd7d396d45efa3888693
GoldAik <aik0124k@gmail.com>
2025-08-20 20:08:15 +0200

    add default route

--- src/Routes/main.php
+++ src/Routes/main.php
@@ -5,23 +5,28 @@ declare(strict_types = 1);
 namespace App\Routes;
 
 use App\Config;
-
+use Psr\Http\Message\ResponseInterface as Response;
 
 return function (\Slim\App $app, $args = []) {    
     /**
      * Declaring routes
      */
+    $app->get('/', function (Response $response) {
+        $response->getBody()->write("Hello World");
+        return $response;
+    });
+
     (require_once 'user.php')($app, $args);
 
 
     /**
      * Caching routes
      */
-$container = $app->getContainer();
+    $container = $app->getContainer();
     $debugMode = $container->get(Config::class)['debug_mode'];
 
     if (! $debugMode) {
-    $routeCollector = $app->getRouteCollector();
-    $routeCollector->setCacheFile(CACHE_PATH . '/routes.file');
-}
+        $routeCollector = $app->getRouteCollector();
+        $routeCollector->setCacheFile(CACHE_PATH . '/routes.file');
+    }
 };
